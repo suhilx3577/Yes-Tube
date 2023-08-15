e355f2f7a84b02530c52deabf3b12f928fe1ffc3
suhilx3577 <suhilx3577@gmail.com>
2023-08-15 20:32:38 +0530

    buiilding custom hook for query results

--- src/components/ButtonList.js
+++ src/components/ButtonList.js
@@ -7,13 +7,11 @@ const ButtonList = () => {
 
   const arr = ['Shreya Goshal', 'Web Development', 'Namaste JS','1-D', 'PUBG', 'Music','One Direction','Breakup Songs', 'Cricket','DSA' ]
   const [query,setQuery] = useState(null)
-  // console.log('button rendered')
 
   async function getQueryResults(){
     const d = await fetch(`https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&type=video&q=${query}&key=${process.env.YOUTUBE_API_KEY}`);
     const data = await d.json();
     dispatch(changeContainer(data.items))
-    // console.log(data.items)
   }
 
   useEffect(()=>{
--- src/components/CardContainer.js
+++ src/components/CardContainer.js
@@ -4,22 +4,7 @@ import { Link } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 const CardContainer = () => {
 
-  // const [ data, setData] = useState('')
-
   const data =  useSelector((state)=>state.container.cdata)
-  // const clink = useSelector((store)=>store.container.clink)
-
-  // async function fetchData (){
-  //   const d = await fetch(process.env.YOUTUBE_API_URLANDKEY);
-  //   const j = await d.json();
-  //   setData(j.items)
-  //   console.log(j.items)
-
-  // }
-  // useEffect(()=>{
-  //   fetchData()
-  // },[])
-
 
   return (
     <div className='grid grid-cols-3 gap-2 mt-2'>
